3e5c7df14825702857fb291e8dd14089d0a4fe6b
Timoth18 <timothyaldre@gmail.com>
2026-05-10 00:27:36 +0700

    no need check, it works trust

--- db.js
+++ db.js
@@ -5,10 +5,6 @@ const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_KEY = process.env.SUPABASE_SERVICE_KEY;
 const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY; // or SERVICE_ROLE_KEY on server
 
-if (!SUPABASE_URL || !SUPABASE_KEY) {
-  throw new Error('Missing SUPABASE_URL or SUPABASE_KEY in environment');
-}
-
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
   auth: { persistSession: false },
 });
