893336d2dec77329429628b27399e9e371fea712
Timoth18 <timothyaldre@gmail.com>
2026-05-10 00:02:45 +0700

    move db

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 import 'dotenv/config';
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_KEY = process.env.SUPABASE_SERVICE_KEY;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY; // or SERVICE_ROLE_KEY on server
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY; // or SERVICE_ROLE_KEY on server
 
 if (!SUPABASE_URL || !SUPABASE_KEY) {
   throw new Error('Missing SUPABASE_URL or SUPABASE_KEY in environment');
